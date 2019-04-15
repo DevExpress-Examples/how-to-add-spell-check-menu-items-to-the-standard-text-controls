a2ba4c2435718d2b69d60a66be2d17554ef5e7d0
Poline Fedorova <polina.fedorova@devexpress.com>
2019-04-15 12:17:40 +0300

    update projects

--- VB/WpfApp1/MainWindow.xaml.vb
+++ VB/WpfApp1/MainWindow.xaml.vb
@@ -17,46 +17,53 @@ Imports DevExpress.Xpf.SpellChecker
 Imports DevExpress.XtraSpellChecker.Parser
 
 Namespace WpfApp1
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits System.Windows.Window
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Partial Public Class MainWindow
+        Inherits System.Windows.Window
 
-		Public Sub New()
-			DataContext = Me
-			InitializeComponent()
-		End Sub
+        Public Sub New()
+            DataContext = Me
+            InitializeComponent()
+        End Sub
 
-		Public ReadOnly Property MenuItems() As ObservableCollection(Of Object)
-		= New ObservableCollection(Of Object)()
+        Public ReadOnly Property MenuItems() As ObservableCollection(Of Object) = New ObservableCollection(Of Object)()
 
-		void RichTextBox_PreviewMouseRightButtonUp(Object sender, MouseButtonEventArgs e)
-			MenuItems.Clear()
-			Dim commands = Me.richTextBox.GetErrorOperationCommands(e.GetPosition(Me.richTextBox))
-			For Each command In commands
-				Dim menuItem = New MenuItem()
-				menuItem.Header = command.Caption
-				AddHandler menuItem.Click, Sub(s, args) command.DoCommand()
-				menuItem.IsEnabled = command.Enabled
-				MenuItems.Add(menuItem)
-			Next command
-			If MenuItems.Count = 0 Then
-				MenuItems.Add(New MenuItem() With {.Header = "No Error", .IsEnabled = False})
-			End If
+        Private Sub RichTextBox_PreviewMouseRightButtonUp(sender As Object, e As MouseButtonEventArgs)
+            MenuItems.Clear()
+            Dim commands = Me.richTextBox.GetErrorOperationCommands(e.GetPosition(Me.richTextBox))
 
-		void TextBox_PreviewMouseRightButtonUp(Object sender, MouseButtonEventArgs e)
-			MenuItems.Clear()
-			Dim commands = Me.textBox.GetErrorOperationCommands(Me.textBox.SelectionStart)
-			For Each command In commands
-				Dim menuItem = New MenuItem()
-				menuItem.Header = command.Caption
-				AddHandler menuItem.Click, Sub(s, args) command.DoCommand()
-				menuItem.IsEnabled = command.Enabled
-				MenuItems.Add(menuItem)
-			Next command
-			If MenuItems.Count = 0 Then
-				MenuItems.Add(New MenuItem() With {.Header = "No Error", .IsEnabled = False})
-			End If
-	End Class
+            For Each richEditCommand In commands
+                Dim menuItem = New MenuItem()
+                menuItem.Header = richEditCommand.Caption
+
+                AddHandler menuItem.Click, Sub(s, args) richEditCommand.DoCommand()
+
+                menuItem.IsEnabled = richEditCommand.Enabled
+                MenuItems.Add(menuItem)
+            Next
+
+            If MenuItems.Count = 0 Then MenuItems.Add(New MenuItem() With {
+            .Header = "No Error",
+            .IsEnabled = False
+        })
+        End Sub
+
+        Private Sub TextBox_PreviewMouseRightButtonUp(sender As Object, e As MouseButtonEventArgs)
+            MenuItems.Clear()
+            Dim commands = Me.textBox.GetErrorOperationCommands(Me.textBox.SelectionStart)
+            For Each richEditCommand In commands
+                Dim menuItem = New MenuItem()
+                menuItem.Header = richEditCommand.Caption
+                AddHandler menuItem.Click, Sub(s, args) richEditCommand.DoCommand()
+                menuItem.IsEnabled = richEditCommand.Enabled
+                MenuItems.Add(menuItem)
+            Next
+
+            If MenuItems.Count = 0 Then
+                MenuItems.Add(New MenuItem() With {.Header = "No Error", .IsEnabled = False})
+            End If
+        End Sub
+    End Class
 End Namespace
